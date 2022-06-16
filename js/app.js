b8d7b4a26c005260c4b8ac804bd6b09748eb4fff
Josh Boyles <jboycr@yahoo.com>
2022-06-16 11:58:34 -0500

    add function to user name input

--- js/app.js
+++ js/app.js
@@ -1,2 +1,24 @@
 'use strict';
 console.log('app is connected.');
+
+let userInput;
+
+function userName(){
+
+
+    userInput = prompt('What is your name?');
+    console.log('The user name: ',userInput);
+
+    return userInput;
+
+
+}
+
+
+
+userName();
+
+
+console.log(userInput);
+
+document.write(userInput + ' Hello');
